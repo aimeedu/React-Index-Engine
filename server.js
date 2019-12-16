714e891ae75251b21d5410ac836ce2165469b675
aimeedu <aimeedu11@gmail.com>
2019-12-16 09:55:46 -0500

    2nd

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const crawler = require('./WebScape');
 
 if (process.env.NODE_ENV === 'production'){
 
-    app.use(express.static(path.join(__dirname,'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 
 }
