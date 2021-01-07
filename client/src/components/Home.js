dcda704d4e205a4340b8c05b9729c217a8c8c761
Aimee <aimee@Aimees-MBP.fios-router.home>
2021-01-06 19:36:20 -0500

    fixed typo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,11 +14,11 @@ const Home = () => {
             <ul id="homelist">
                 
                 <h4>Instructions:</h4>
-                <li>Click on 'Indexing Luncher', then you can input a web page to index all the words from that page.</li>
-                <li>Click on 'Custom Search Engine' to search the words in the database you have priviously idexed.</li>
+                <li>Click on 'Indexing Launcher', then you can input a web page to index all the words from that page.</li>
+                <li>Click on 'Custom Search Engine' to search the words in the database you have priviously indexed.</li>
                 <br/>
                 Dropdown Menus: 
-                <li>Search: Search from file or search from google.</li>
+                <li>Search: Search from a file or search from google.</li>
                 <li>Browser: Display your browser sepcification. </li>
                 <li>Geolocation: Displays your current latitude and longitude.</li>
                 <li>About: Developer's names and you can contact us by email.</li>
